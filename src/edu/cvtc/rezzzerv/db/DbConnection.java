522b0b09d5c6293082026d5a0e5ccb9ab6ecc4c1
Rainer Rodriguez <rrodriguezalfaro@student.cvtc.edu>
2025-04-25 00:03:22 -0500

    cloud db connection and jar to read .env files

--- src/edu/cvtc/rezzzerv/db/DbConnection.java
+++ src/edu/cvtc/rezzzerv/db/DbConnection.java
@@ -1,12 +1,14 @@
 package edu.cvtc.rezzzerv.db;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbConnection {
 
-    private static final String URL = "jdbc:sqlite:ReZZZerv.db";
     private static Connection connection = null;
 
     private DbConnection() {
@@ -15,8 +17,23 @@ public class DbConnection {
     public static Connection getConnection() {
         try {
             if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(URL);
-                System.out.println("Connection established");
+                Dotenv dotenv = Dotenv.load();
+
+                String DB_HOST = dotenv.get("DB_HOST");
+                String DB_NAME = dotenv.get("DB_NAME");
+                String USER = dotenv.get("DB_USER");
+                String PASSWORD = dotenv.get("DB_PASSWORD");
+
+                Properties properties = new Properties();
+                properties.setProperty("user", USER);
+                properties.setProperty("password", PASSWORD);
+                properties.setProperty("sslMode", "VERIFY_IDENTITY");
+                properties.setProperty("enabledTLSProtocols", "TLSv1.2");
+
+                String url = "jdbc:mysql://" + DB_HOST + "/" + DB_NAME;
+
+                connection = DriverManager.getConnection(url, properties);
+                System.out.println("PlanetScale connection established");
             }
         } catch (SQLException e) {
             System.out.println("Error trying to connect to the database");
