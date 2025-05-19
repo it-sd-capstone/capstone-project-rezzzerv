c0a37610e3d080436da13b41652aaeb09e73aa78
Silong Xiong <sxiong63@student.cvtc.edu>
2025-05-19 17:51:56 -0500

    Removed old commented code

--- src/main/java/controller/PaymentServlet.java
+++ src/main/java/controller/PaymentServlet.java
@@ -39,14 +39,7 @@ public class PaymentServlet extends HttpServlet {
         }
 
         // Get form parameters
-        //String cardName = request.getParameter("cardName");
         String cardNumber = request.getParameter("cardNumber");
-        // String expDate = request.getParameter("expDate");
-        //String cvv = request.getParameter("cvv");
-        //String billingAddress = request.getParameter("billingAddress");
-        //String city = request.getParameter("city");
-        //String zipCode = request.getParameter("zipCode");
-        //String country = request.getParameter("country");
         String amountStr = request.getParameter("amount");
         String dateStr = request.getParameter("paidDate");
 
@@ -62,11 +55,6 @@ public class PaymentServlet extends HttpServlet {
         LocalDate paidDate = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
 
-        // In a real application, you would:
-        // 1. Validate the payment information
-        // 2. Process the payment through a payment gateway
-        // 3. Update the reservation status in the database
-
         Payment payment;
         try {
             payment = paymentService.payByReserveId(cardNumber, amount, paidDate, reserveId);
@@ -80,19 +68,6 @@ public class PaymentServlet extends HttpServlet {
         session.removeAttribute("reserve");
 
         response.sendRedirect("reservationConfirmation.jsp");
-
-        // For now, we'll just simulate a successful payment
-        //HttpSession session = request.getSession();
-
-        // Get test parameter if it exists
-//        String testParam = request.getParameter("test");
-//
-//        // Forward to the confirmation page
-//        if (testParam != null && testParam.equals("true")) {
-//            response.sendRedirect("reservationConfirmation.jsp?test=true");
-//        } else {
-//            response.sendRedirect("reservationConfirmation.jsp");
-//        }
     }
 
 
